f25b8737d99e80bff2bed88fe9156e5feac727b6
Lamia EL RALIMI <lamia.elralimi@nova-technology.fr>
2020-10-09 17:19:57 +0200

    Added update method to app.py to update tasks

--- app.py
+++ app.py
@@ -13,14 +13,25 @@ class Task(db.Model):  # Modèle pour une tâche
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])  # méthodes autorisées sur ma route
 def index():  # Vue dans laquel on traite les données du formulaire
-    if request.method == 'POST':
-        name = request.form.get('name')
-        task = Task(name=name)
-        db.session.add(task)
-        db.session.commit()
-        return redirect(url_for('index'))
+    if request.method == 'POST':  # indiquer au paramètre 'methods' routes autorisées
+        name = request.form.get('name')  # récup infos du formulaire grâce à request
+        task = Task(name=name)  # créer tâche ds ma bdd en passant par Model Task
+        db.session.add(task)  # ajouter objet 'task' à la session
+        db.session.commit()  # enregistrer objet à la session
+        return redirect(url_for('index'))  # rediriger l'utilisateur vers page 'index' grâce à url_for qui envoie vers vue
     else:
         tasks = Task.query.order_by(Task.created_at).all()
     return render_template('index.html', tasks=tasks)
+
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update():
+    task = Task.query.get_or_404(id)  # on récupère la tâche dans notre bdd grâce à l'id
+    if request.method == 'UPDATE':
+        task.name = request.form.get('name')  # on màj le nom de la tâche avec les données reçues du formulaire
+        db.session.commit()
+        return redirect(url_for('index'))
+    else:
+        return render_template('index.html', task=task)
