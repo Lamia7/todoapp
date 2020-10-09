4bfee8024aca2674b1d16880779641c1f142544f
Lamia EL RALIMI <lamia.elralimi@nova-technology.fr>
2020-10-09 14:26:42 +0200

    Created db and Task table

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
-app = Flask(__name__)  # Création d'instance de Flask : notre app
+app = Flask(__name__) # Crée un instance de la classe Flask, c'est notre app
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db' # Nom de la bdd
+db = SQLAlchemy(app) # Lie notre app à SQLAlchemy
+
+
+class Task(db.Model): # Modèle
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
 
 
 @app.route('/')
-def index():  # Méthode appelée depuis la route /
-    return "Hello World!"
-
-
-if __name__ == "__main__":
-    app.run()
+def index(): # Méthode appelée lorsqu'on se rend sur la route '/'
+    return 'Hello World!'
